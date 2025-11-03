48f174c1f1076ae2e54f709112502b9ebb5a96b8
unsubstantiated-Script <jusfulton@gmail.com>
2025-11-02 18:45:06 -0800

    Initial API config

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,23 @@
-def main():
-    print("Hello from backend!")
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
+app = FastAPI(
+    title="Choose Your Own Adventure Game API",
+    description="An API for a choose your own adventure game where users can make choices to navigate through a story.",
+    verson="0.1.0",
+    docs="/docs",
+    redoc="/redoc"
+)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Using uvicorn to run the API app
 if __name__ == "__main__":
-    main()
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
